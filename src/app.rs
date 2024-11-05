c22e28e985c599da3fee3634d323dc4a2fd608cd
Vincent Winkler <vincentius.winkler@gmail.com>
2024-11-05 11:29:36 +0100

    did smth idk

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,42 @@
 use leptos::*;
 use leptos_mview::mview;
+use leptos_icons::*;
+use icondata as i;
+use icondata_core::IconData;
+
+#[derive(Clone)]
+enum SelectedRoute {
+    Home,
+    Devices,
+    Alarm
+}
+
+#[derive(Clone)]
+struct RouteSetter(WriteSignal<SelectedRoute>);
 
 #[component]
 pub fn App() -> impl IntoView {
+    let (selected_route, set_selected_route) = create_signal(SelectedRoute::Home);
+    provide_context(RouteSetter(set_selected_route));
+
     mview! {
-        button class="btn btn-primary"("Wuhu")
+        {match selected_route() {
+            SelectedRoute::Home => mview! {"Home"},
+            SelectedRoute::Devices => mview! {"Devices"},
+            SelectedRoute::Alarm => mview! {"Alarm"},
+        }}
+        div class="btm-nav" {
+            BtmNavItem text={"Home"} icon={i::AiHomeOutlined}()
+            BtmNavItem text={"Devices"} icon={i::BiDevicesRegular}()
+            BtmNavItem text={"Alarm"} icon={i::ChClockAlarm}()
+        }
+    }
+}
+
+#[component]
+fn BtmNavItem(text: &'static str, icon: &'static IconData) -> impl IntoView {
+    let ssr = use_context::<RouteSetter>().unwrap();
+    mview! {
+        button onclick=[ssr.0] class="text-primary" { Icon icon={icon}() span class="btm-nav-label"({text})}
     }
 }
