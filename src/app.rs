09d939f3b7156845e6fb9bcf0f37be4ce495bba3
mageOfStructs <jason.puschnig@gmx.at>
2024-12-09 15:04:17 +0100

    fixed your stuff

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,30 @@
-use leptos::*;
-use leptos_mview::mview;
-use leptos_icons::*;
+use std::fmt::Display;
+
 use icondata as i;
 use icondata_core::IconData;
+use leptos::*;
+use leptos_icons::*;
+use leptos_mview::mview;
 
 #[derive(Clone)]
 enum SelectedRoute {
     Home,
     Devices,
-    Alarm
+    Alarm,
+}
+
+impl Display for SelectedRoute {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Alarm => "Alarm",
+                Self::Devices => "Devices",
+                Self::Home => "Home",
+            }
+        )
+    }
 }
 
 #[derive(Clone)]
@@ -20,28 +36,21 @@ pub fn App() -> impl IntoView {
     provide_context(RouteSetter(set_selected_route));
 
     mview! {
-        button()
-        button()
-        button()
-        {match selected_route() {
-            SelectedRoute::Home => mview! {"Home"},
-            SelectedRoute::Devices => mview! {"Devices"},
-            SelectedRoute::Alarm => mview! {"Alarm"},
-        }}
+        {move || selected_route().to_string()}
         div class="btm-nav" {
-            BtmNavItem text={"Home"} icon={i::AiHomeOutlined}()
-            BtmNavItem text={"Devices"} icon={i::BiDevicesRegular}()
-            BtmNavItem text={"Alarm"} icon={i::ChClockAlarm}()
-            button()
-            button()
+            BtmNavItem route={SelectedRoute::Home} icon={i::AiHomeOutlined}()
+            BtmNavItem route={SelectedRoute::Devices} icon={i::BiDevicesRegular}()
+            BtmNavItem route={SelectedRoute::Alarm} icon={i::ChClockAlarm}()
         }
     }
 }
 
 #[component]
-fn BtmNavItem(text: &'static str, icon: &'static IconData) -> impl IntoView {
+fn BtmNavItem(route: SelectedRoute, icon: &'static IconData) -> impl IntoView {
     let ssr = use_context::<RouteSetter>().unwrap();
+    let moved_route = route.clone(); // no idea why it wants two copies, with normal view! I only
+                                     // need one
     mview! {
-        // button onclick=[ssr.0] class="text-primary" { Icon icon={icon}() span class="btm-nav-label"({text})}
+        button on:click={move |_| ssr.0(moved_route.clone())} class="text-primary" { Icon icon={icon}() span class="btm-nav-label"({route.to_string()})}
     }
 }
