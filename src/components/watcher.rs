6a6d044b4e669d33374c8181cb9fb0a0c4528eda
mageOfStructs <jason.puschnig@gmx.at>
2024-12-13 10:33:35 +0100

    add button group

--- src/components/watcher.rs
+++ src/components/watcher.rs
@@ -1,8 +1,34 @@
 use leptos::component;
+use leptos::create_signal;
 use leptos::IntoView;
+use leptos::ReadSignal;
+use leptos::SignalGet;
 use leptos_mview::mview;
+
+#[derive(Clone, PartialEq)]
+enum WatcherRoute {
+    Blacklist,
+    Whitelist,
+}
+
+fn get_classes(route: ReadSignal<WatcherRoute>, desired: WatcherRoute) -> String {
+    format!(
+        "btn join-item flex-1 rounded-l-lg{}",
+        if route() == desired {
+            " button-primary"
+        } else {
+            ""
+        }
+    )
+}
 
 #[component]
 pub fn Watcher() -> impl IntoView {
-    mview! {}
+    let (route, set_route) = create_signal(WatcherRoute::Blacklist);
+    mview! {
+        div class="join flex" {
+            button class={move || get_classes(route, WatcherRoute::Blacklist)} { "Blacklist" }
+            button class={move || get_classes(route, WatcherRoute::Whitelist)} { "Whitelist" }
+        }
+    }
 }
