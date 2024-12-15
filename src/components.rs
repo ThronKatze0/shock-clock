84ade47cdbdfa44fd045619e4822f404244d62af
mageOfstructs <jason.puschnig@gmx.at>
2024-12-15 17:12:10 +0100

    add non-working icon logic

--- src/components.rs
+++ src/components.rs
@@ -1,9 +1,71 @@
+use leptos::on_cleanup;
+use leptos::set_interval_with_handle;
+use leptos::Signal;
+use std::time::Duration;
+
+use icondata as i;
+use icondata::Icon;
 use leptos::component;
+use leptos::create_signal;
+use leptos::set_interval;
 use leptos::IntoView;
+use leptos_icons::Icon;
 use leptos_mview::mview;
+
+use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::to_value;
+use wasm_bindgen::prelude::*;
+
+#[derive(Serialize, Deserialize)]
+struct ShockArgs {
+    duration: u16,
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
+
+enum Status {
+    Offline,
+    Online,
+}
 
 #[component]
 pub fn Home() -> impl IntoView {
+    let shock_test = move |_| {
+        spawn_local((async move || {
+            let args = to_value(&ShockArgs { duration: 1000 }).unwrap();
+            invoke("shock", args).await;
+        })());
+    };
+    let (clock_stat, set_clock_stat) = create_signal(false);
+    let handle = set_interval_with_handle(
+        move || {
+            let stat_clone = set_clock_stat.clone();
+            let async_closure = async move || {
+                stat_clone(
+                    invoke("is_connected", JsValue::undefined())
+                        .await
+                        .as_bool()
+                        .unwrap(),
+                );
+            };
+            spawn_local(async_closure());
+        },
+        Duration::from_secs(5),
+    );
+    on_cleanup(move || {
+        handle;
+    });
+    let get_icon = move || {
+        if clock_stat() {
+            i::AiCheckOutlined
+        } else {
+            i::AiCloseOutlined
+        }
+    };
     mview! {
         div class="prose h-screen" {
             h1 class="my-4" { "Shock Clock" }
@@ -14,12 +76,13 @@ pub fn Home() -> impl IntoView {
                     }
                     div class="stat" {
                         span class="stat-title" { "Clock" }
+                        Icon icon={Signal::derive(get_icon)};
                     }
                 }
 
                 div class="flex flex-auto h-6/16 pt-48" {
                     div class="flex-1";
-                    button class="btn center text-6xl rounded-full flex-auto h-4/6 w-1/12 border-yellow-500 border-4" {"⚡"}
+                    button on:click={shock_test} class="btn center text-6xl rounded-full flex-auto h-4/6 w-1/12 border-yellow-500 border-4" {"⚡"}
                     div class="flex-1";
                 }
                 div class="flex-1 form-control" {
@@ -43,6 +106,7 @@ pub fn Home() -> impl IntoView {
 
 pub mod watcher;
 
+use wasm_bindgen_futures::spawn_local;
 pub use watcher::Watcher;
 
 #[component]
